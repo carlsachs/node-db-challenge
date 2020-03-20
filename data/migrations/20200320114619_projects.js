870679f3bb123940eb93e6271f1223dae0465d49
carlsachs <sachscarl@gmail.com>
2020-03-20 12:43:22 -0400

    fixed my bug

--- data/migrations/20200320114619_projects.js
+++ data/migrations/20200320114619_projects.js
@@ -1,8 +1,43 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('projects', tbl => {
+        tbl.increments();
+
+        tbl.varchar('name', 100).notNullable()
+
+        tbl.text('description', 250)
+        
+        tbl.boolean('Completed', false)
+    })
+    .createTable('resources', tbl => {
+        tbl.increments()
+
+        tbl.varchar('name', 50).notNullable()
+
+        tbl.text('description', 250)
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+
+        tbl.integer('projects_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+
+        tbl.text("description", 250)
+
+        tbl.text("notes", 250)
+
+        tbl.boolean("completed", false)
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
 };
